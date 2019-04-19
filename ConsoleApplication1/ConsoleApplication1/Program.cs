12aa4e7c4c1330ec96111fc6543f9629a24936f2
kingster307 <33669832+kingster307@users.noreply.github.com>
2019-04-19 11:23:34 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 100; i >= 1; ++i)
+            {
+                Console.WriteLine(i);
+                // console.writeline(i);
+            }
+        }
+    }
+}
---
+++ Fourth_two_Weeks_num1/Fourth_two_Weeks_num1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Fourth_two_Weeks_num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int smallest=0,x=0,c=0;
+            Console.WriteLine("How many numbers would you like to input?");
+           int number = int.Parse(Console.ReadLine());
+           int[] AllNumbers = new int[number];
+            while (x < number)
+            {
+                Console.WriteLine("{0}) Please Enter a number", number-number +(x+1));
+                AllNumbers[x] = int.Parse(Console.ReadLine());
+                x++;
+            }
+            smallest = AllNumbers[0];
+            for (int y = 1; y < number; y++)
+            {
+                if (smallest > AllNumbers[y])
+                {
+                    smallest = AllNumbers[y];
+                }
+            }
+           Console.WriteLine("Smallest number: {0}", smallest);
+           Console.Read();
+        }
+    }
+}
